2cd2c28272ba7e376ec3cf0e455af66fd4c0175a
bibi.star <bianca.nunes.bacharel@gmail.com>
2020-11-18 22:01:57 -0300

    Alteração

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.port || 8080;
+const port = process.env.port || 5000;
 const host = process.env.host || '0.0.0.0';
 
 server.use(middlewares);
@@ -13,5 +13,5 @@ server.use(router);
 server.listen(port,host,()=>
 {
     console.log('Json Server is running in ',port);
-    
+
 });
