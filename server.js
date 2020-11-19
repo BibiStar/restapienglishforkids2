27f8841b1b7cdfbcd747990ade7ce3479948b3de
bibi.star <bianca.nunes.bacharel@gmail.com>
2020-11-18 22:23:55 -0300

    Alteração

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.port || 8080;
+const port = process.env.port || 5555;
 const host = process.env.host || '0.0.0.0';
 
 server.use(middlewares);
